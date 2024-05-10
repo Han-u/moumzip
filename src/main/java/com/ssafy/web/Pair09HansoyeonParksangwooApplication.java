16c5eec1336d32d3537223c87c756e4b211cf7e7
han <chos9715@naver.com>
2024-05-10 16:51:13 +0900

    Init

---
+++ src/main/java/com/ssafy/web/Pair09HansoyeonParksangwooApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pair09HansoyeonParksangwooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pair09HansoyeonParksangwooApplication.class, args);
+	}
+
+}
