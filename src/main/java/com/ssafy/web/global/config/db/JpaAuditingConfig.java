ea351318a0621cf05de0a4cca97f229517747e82
han <chos9715@naver.com>
2024-05-19 03:46:54 +0900

    feat: User 정보 추출 유틸 추가

--- src/main/java/com/ssafy/web/global/config/db/JpaAuditingConfig.java
+++ src/main/java/com/ssafy/web/global/config/db/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.web.global.config;
+package com.ssafy.web.global.config.db;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
