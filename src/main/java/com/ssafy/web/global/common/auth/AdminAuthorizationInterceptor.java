2150483a8a3ed54c426813e8d134e0e4d6309af4
han <chos9715@naver.com>
2024-05-21 20:05:31 +0900

    fix: 인증 후 admin 아닌데 DB 확인해야하는 케이스 수정

--- src/main/java/com/ssafy/web/global/common/auth/AdminAuthorizationInterceptor.java
+++ src/main/java/com/ssafy/web/global/common/auth/AdminAuthorizationInterceptor.java
@@ -1,5 +1,6 @@
 package com.ssafy.web.global.common.auth;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -19,11 +20,26 @@ import lombok.RequiredArgsConstructor;
 public class AdminAuthorizationInterceptor implements HandlerInterceptor {
 	private final JwtTokenProvider jwtTokenProvider;
 	private final AuthService authService;
+
+	public boolean hasCurrentUser(HandlerMethod handlerMethod){
+		for(MethodParameter parameter: handlerMethod.getMethodParameters()){
+			if(parameter.hasParameterAnnotation(CurrentUser.class)){
+				return true;
+			}
+		}
+		return false;
+	}
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		if(!(handler instanceof HandlerMethod handlerMethod)){
 			return true;
 		}
+
+		RequiresAdmin requireMethod = handlerMethod.getMethodAnnotation(RequiresAdmin.class); // method에 @RequiresAdmin 여부
+		boolean requireClass = handlerMethod.getMethod().getDeclaringClass().isAnnotationPresent(RequiresAdmin.class); // class에 @RequiresAdmin 여부
+
+		boolean checkUser = hasCurrentUser(handlerMethod);
+		boolean requireAdmin = requireClass || requireMethod != null;
 
 		// 토큰 검증
 		String token = jwtTokenProvider.resolveToken(request);
@@ -32,19 +48,17 @@ public class AdminAuthorizationInterceptor implements HandlerInterceptor {
 			throw new BusinessException(ErrorCode.UNAUTHORIZED);
 		}
 
-		RequiresAdmin requireMethod = handlerMethod.getMethodAnnotation(RequiresAdmin.class); // method에 @RequiresAdmin 여부
-		boolean requireClass = handlerMethod.getMethod().getDeclaringClass().isAnnotationPresent(RequiresAdmin.class); // class에 @RequiresAdmin 여부
 
 		// 멤버 가져오기
 		Member member = jwtTokenProvider.extractMember(token);
 
-		if(requireMethod == null && !requireClass) { // @RequiresAdmin이 붙어있지 않다면 패스
+		if(!requireAdmin && !checkUser) { // @RequiresAdmin이 붙어있지 않다면 패스
 			request.setAttribute("member", member);
 			return true;
 		}
 
 		member = authService.getMemberById(member.getMemberId());
-		if(!member.isAdmin()){ // 권한이 없는 경우
+		if(requireAdmin && !member.isAdmin()){ // 권한이 없는 경우
 			throw new BusinessException(ErrorCode.FORBIDDEN);
 		}
 
