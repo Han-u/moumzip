b84403ddc2dcbe9de017910a01bd62555159b6c4
han <chos9715@naver.com>
2024-05-21 20:06:18 +0900

    fix: RequestBody 등 어노테이션 빼먹은 부분 수정

--- src/main/java/com/ssafy/web/domain/deposit/controller/DepositController.java
+++ src/main/java/com/ssafy/web/domain/deposit/controller/DepositController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,7 @@ public class DepositController {
 	}
 
 	@PostMapping("/{auctionId}/otp")
-	public ResponseEntity<?> setAuctionOtp(@CurrentUser Member member, @PathVariable Long auctionId, OtpRequest otpRequest){
+	public ResponseEntity<?> setAuctionOtp(@CurrentUser Member member, @PathVariable Long auctionId, @RequestBody OtpRequest otpRequest){
 		depositService.setOtp(member, auctionId, otpRequest);
 		return ResponseEntity.status(HttpStatus.OK).build();
 	}
