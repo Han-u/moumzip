e45446282cc7804b42ce5cdf3980fb78e532a068
han <chos9715@naver.com>
2024-05-23 10:23:58 +0900

    feat: 입찰 구현

---
+++ src/main/java/com/ssafy/web/global/common/db/Retry.java
@@ -0,0 +1,13 @@
+package com.ssafy.web.global.common.db;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Retry {
+	int maxRetries() default 1000;
+	int retryDelay() default 100;
+}
